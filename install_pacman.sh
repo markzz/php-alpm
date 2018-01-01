636ab360f041f4465ec5d2848c1c24125dbbf902
Mark Weiman <mark.weiman@markzz.com>
2017-12-31 23:20:13 -0500

    Add pacman git support to travis
    Signed-off-by: Mark Weiman <mark.weiman@markzz.com>

---
+++ install_pacman.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+echo "Building pacman version ${1}"
+
+case $1 in
+    git)
+        git clone http://git.archlinux.org/pacman.git pacman-git
+        ;;
+    *)
+        wget -O pacman-${1}.tar.gz https://sources.archlinux.org/other/pacman/pacman-${1}.tar.gz
+        tar -xzf pacman-${1}.tar.gz
+        ;;
+esac
+
+if [ -x "$(command -v apt-get)" ]; then
+    sudo apt-get -qq update
+    sudo apt-get install -y autopoint pkg-config libtool gettext libarchive-dev curl libgpgme11-dev
+else
+    exit 1
+fi
+
+cd pacman-${1}
+set +e
+if [ "${1}" == "git" ]; then
+    ./autogen.sh
+fi
+./configure --prefix=/usr --disable-doc
+make
+sudo make install
